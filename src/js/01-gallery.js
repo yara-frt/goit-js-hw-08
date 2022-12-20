888d69375762d1c9c4dced66480b04962f463d57
Yaroslav Ashchaulov <y.ashchaulov@gmail.com>
2022-12-20 23:00:36 +0200

    added task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce((acc, { preview, original, description }) => acc += `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`, '');
+list.insertAdjacentHTML('beforeend', markup);
+
+let originalGallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
