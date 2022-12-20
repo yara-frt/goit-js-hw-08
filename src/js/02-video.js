d74daec1fea19c99c3b160fa2dff5e9455560494
Yaroslav Ashchaulov <y.ashchaulov@gmail.com>
2022-12-20 23:16:03 +0200

    added task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const a = 2;
+console.log(a);
+
+player.on('timeupdate', throttle(getTime,1000));
+
+function getTime(event) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(event.seconds));
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
