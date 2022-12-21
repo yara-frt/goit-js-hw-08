84c2cc98a30982a4d258c6236c0aae70e767c1c8
Yaroslav Ashchaulov <y.ashchaulov@gmail.com>
2022-12-21 23:22:51 +0200

    added task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,6 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const a = 2;
-console.log(a);
 
 player.on('timeupdate', throttle(getTime,1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const formData = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+
+updateData();
+
+formData.form.addEventListener('submit', onFormSubmit);
+formData.form.addEventListener('input', throttle(onInput, 500));
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const dataJson = localStorage.getItem("feedback-form-state");
+    const dataObject = JSON.parse(dataJson);
+    console.log(dataObject);
+
+    event.currentTarget.reset();
+
+    localStorage.removeItem("feedback-form-state");
+
+}
+
+function onInput(event) {
+    const { elements: { email, message }, } = event.currentTarget;
+    const allData = {
+        userEmail: email.value,
+        userMessage: message.value,
+    };
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(allData));
+}
+
+
+function updateData() {
+    const getDataJson = localStorage.getItem("feedback-form-state");
+    const getDataUser = JSON.parse(getDataJson);
+
+    formData.form.elements.email.value = getDataUser?.userEmail || '';
+    formData.form.elements.message.value = getDataUser?.userMessage || '';
+    
+}
